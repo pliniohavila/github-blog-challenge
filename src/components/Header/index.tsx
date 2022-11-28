98b8ac1a8682f4f455df2f9a3179f70c0fbfa382
pliniocode <pliniohrcode@gmail.com>
2022-11-28 17:49:13 -0300

    config theme

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { HeaderComponent } from "../styles";
+
+
+export function Header() {
+  return (
+    <HeaderComponent>
+      <h1>Github blog challenge</h1>
+    </HeaderComponent>
+  )
+}
---
+++ src/components/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const HeaderComponent = styled.header`
+  color: ${(props) => props.theme['base-profile']};
+  background-color: ${(props) => props.theme['base-subtitle']};
+`;
