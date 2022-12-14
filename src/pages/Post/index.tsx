b9ddc8e7213b7059230fda497c9444d7fbcbebd3
pliniocode <pliniohrcode@gmail.com>
2022-12-14 16:04:05 -0300

    implement  markdown render

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,41 +1,38 @@
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { ArrowSquareUpRight, Calendar, CaretLeft, ChatsTeardrop, GithubLogo } from "phosphor-react";
 import { PostContent, PostCover } from "./styles";
-import { useEffect, useState } from "react";
 import { api } from "../../lib/axios";
 import { daysBetweenDates } from "../../utils/formatter";
 
 
 type PostInfoType = {
-  html_url: string, 
-  title: string, 
-  user: any, 
-  comments: number, 
-  created_at: string, 
+  html_url: string,
+  title: string,
+  user: any,
+  comments: number,
+  created_at: string,
   body: string
 }
 
 
 export function Post() {
   const [content, setContent] = useState<PostInfoType>();
-  const [notFound, setNotFound] = useState(false);
   const { id } = useParams();
-  const print = console.log.bind(console);
 
   async function fetchIssue(id?: string) {
     if (!id) {
-      setNotFound(true);
       return;
     }
 
     const response = await api.get(`/repos/pliniocode/github-blog-challenge/issues/${id}`);
     if (response.data.message) {
-      setNotFound(true);
       return;
     } else {
-      const {html_url, title, user, comments, created_at, body} = response.data;
-      const data = {html_url, title, user, comments, created_at, body};
-      setNotFound(false);
+      const { html_url, title, user, comments, created_at, body } = response.data;
+      const data = { html_url, title, user, comments, created_at, body };
       setContent(data);
     }
   }
@@ -43,7 +40,6 @@ export function Post() {
   useEffect(() => {
     fetchIssue(id);
   }, []);
-  print(content);
 
   if (!content) {
     return (
@@ -54,31 +50,33 @@ export function Post() {
   } else {
     return (
       <>
-      <PostCover>
-        <div className="post-header">
-          <a href="/">
-            <CaretLeft size={18} weight="bold" className="profile-info-label"/>
-            voltar
-          </a>
-          <a href={content.html_url}>
-            ver no github 
-            <ArrowSquareUpRight size={18} weight="bold" /> 
-          </a>
-        </div>
+        <PostCover>
+          <div className="post-header">
+            <a href="/">
+              <CaretLeft size={18} weight="bold" className="profile-info-label" />
+              voltar
+            </a>
+            <a href={content.html_url}>
+              ver no github
+              <ArrowSquareUpRight size={18} weight="bold" />
+            </a>
+          </div>
 
-        <h1>{content.title}</h1>
+          <h1>{content.title}</h1>
 
-        <ul className="info">
-          <li><GithubLogo size={18} weight="bold" className="post-info"/> {content.user.login}</li>
-          <li><Calendar  size={18} weight="bold" className="post-info"/> Há {daysBetweenDates(content.created_at)} dia(s)</li>
-          <li><ChatsTeardrop size={18} weight="bold" className="post-info"/> {content.comments} comentários</li>
-        </ul>
-      </PostCover>
+          <ul className="info">
+            <li><GithubLogo size={18} weight="bold" className="post-info" /> {content.user.login}</li>
+            <li><Calendar size={18} weight="bold" className="post-info" /> Há {daysBetweenDates(content.created_at)} dia(s)</li>
+            <li><ChatsTeardrop size={18} weight="bold" className="post-info" /> {content.comments} comentários</li>
+          </ul>
+        </PostCover>
 
-      <PostContent>
-        <p>{content.body}</p>
-      </PostContent>
+        <PostContent>
+          <p>
+            <ReactMarkdown remarkPlugins={[remarkGfm]}>{content.body}</ReactMarkdown>
+          </p>
+        </PostContent>
       </>
     )
-  }  
+  }
 }
